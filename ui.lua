ed8cc24cd08ad9b4485c520a70c5df81ea6e3bb3
Fan6 <56741965+471654852@users.noreply.github.com>
2021-11-03 09:24:52 -0600

    Add "restUI" option

--- ui.lua
+++ ui.lua
@@ -2,7 +2,7 @@ require "TSLib"
 --w,h = getScreenSize()
 function mainUi()
     UINew(1,"主功能","确定","取消","save.dat",1,0,700,500,"255,255,255","242,242,242","","dot",1)
-    UIRadio(1,"radio1","自选循环刷图,自选剿灭循环","1",-1,0,"",1,2)
+    UIRadio(1,"radio1","自选循环刷图,自选剿灭循环,restUI","1",-1,0,"",1,2)
     --UIRadio(1,"radio1","一键日常,自选循环刷图,一键日常后刷图,日常循环","1",-1,0,"",1,2)
     UILabel(1,"刷图次数",14,"left","38,38,38")
     UIEdit(1,"text0","刷图次数，无限刷填0或者留空","",15,"left","38,38,38","default")
